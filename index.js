700e868bafcdfb5d81e6943e1c625a33f760786b
Jason Harmon <jason.harmon@gmail.com>
2015-08-16 15:35:33 -0500

    All validation in Joi

--- index.js
+++ index.js
@@ -10,11 +10,19 @@ var Joi     = require("joi");
 
 'use strict';
 
+pathNamingConventions = {
+  "spine-case": /^[\/a-z0-9\-]*$/g,
+  "CAP-SPINE-CASE": /^[\/A-Z0-9\-]*$/g,
+  "snake_case": /^[\/a-z0-9\_]*$/g,
+  "camelCase": /^[\/a-z]+[\/A-Z0-9][\/a-z0-9]+[\/A-Za-z0-9]*$/g,
+  "ProperCase": /^(\/[A-Z][a-z]*)+$/g
+}
+
 namingConventions = {
-  "spine-case": /[^\/a-z0-9\-]*/g,
-  "CAP-SPINE-CASE": /[^\/A-Z0-9\-]*/g,
-  "snake_case": /[^\/a-z0-9\_]*/g,
-  "camelCase": /[\/a-z]+[\/A-Z0-9][\/a-z0-9]+[\/A-Za-z0-9]*$/g,
+  "spine-case": /^[a-z0-9\-]*$/g,
+  "CAP-SPINE-CASE": /^[A-Z0-9\-]*$/g,
+  "snake_case": /^[a-z0-9\_]*$/g,
+  "camelCase": /^[a-z]+[\/A-Z0-9][\/a-z0-9]+[\/A-Za-z0-9]*$/g,
   "ProperCase": /^([A-Z][a-z]*)+$/g
   //"Train-Case": "[a-z0-9\-]*
 }
@@ -47,94 +55,56 @@ function getCheckStylePromise(file) {
   });
 }
 
-function validatePath(path, pathNamingConvention) {
-  matchPath = path.replace(pathNamingConvention, "") === path
-  if (!matchPath) {
-    return new ValidationError("path", path);
-  } else {
-    return null;
-  }
-}
-
-function validateOperation(opId, opNamingConvention) {
-  matchOp = opId.match(opNamingConvention);
-  if (matchOp == null || matchOp[0] != opId) {
-    return new ValidationError("operation", opId);
-  } else {
-    return null;
-  }
-}
-
-function validateConventions(spec, pathNamingConvention, opNamingConvention) {
-  var errors = new Array();
-
-  result = mask(spec, "paths/*/*/operationId");
-  paths = result.paths
-  _.each(Object.keys(paths), function(path) {
-
-    pathError = validatePath(path, pathNamingConvention);
-    if (pathError != null) errors.push(pathError);
-
-    pathValue = result.paths[path]
-    _.each(Object.keys(pathValue), function(verb) {
-
-      opId = pathValue[verb].operationId;
-      opError = validateOperation(opId, opNamingConvention);
-      if (opError != null) errors.push(opError);
-
-    });
-  });
-  return errors;
-}
-
 function getSchema(checkStyle) {
-  pathConvention = namingConventions[checkStyle.paths.namingConvention];
+  pathConvention = pathNamingConventions[checkStyle.paths.namingConvention];
   opIdConvention = namingConventions[checkStyle.paths.operationId.namingConvention];
   tagConvention= namingConventions[checkStyle.paths.tags.namingConvention];
   queryParamConvention = namingConventions[checkStyle.paths.parameters.query.namingConvention];
   headerParamConvention = namingConventions[checkStyle.paths.parameters.header.namingConvention];
   pathParamConvention = namingConventions[checkStyle.paths.parameters.path.namingConvention];
   statuses = new RegExp("^".concat(Object.keys(checkStyle.paths.status).join("$|^")).concat("$"));
+  verbs = new RegExp("^".concat(checkStyle.paths.verbs.join("$|^")).concat("$"));
+
+  parameterKeys = {
+    'in': Joi.string().valid('query', 'header'),
+    format: Joi.string(),
+    type: Joi.string(),
+    description: Joi.string(),
+    required: Joi.boolean(),
+    name: Joi.alternatives()
+      .when('in', {is: 'query', then: Joi.string().regex(queryParamConvention)})
+      .when('in', {is: 'header', then: Joi.string().regex(headerParamConvention)})
+  }
 
   schema = Joi.object().keys({
     swagger: Joi.any().valid(checkStyle.swagger),
-    info: Joi.any(),
+    info: Joi.object().keys({
+      title: Joi.string(),
+      description: Joi.string(),
+      version: Joi.string()
+    }),
     host: joiRegex(checkStyle.host),
     scheme: joiRegex(checkStyle.schemes),
     basePath: joiRegex(checkStyle.basePath),
     produces: Joi.array().items(joiRegex(checkStyle.produces)),
     consumes: Joi.array().items(joiRegex(checkStyle.consumes)),
     schemes: Joi.array().items(joiRegex(checkStyle.schemes)),
-    paths: Joi.object() .pattern(pathConvention, Joi.object().keys({})
-    // TODO: pull list of verbs into regexp
-    .pattern(new RegExp("get"), Joi.object().keys({
-      summary: Joi.any(),
-      description: Joi.any(),
-      parameters: Joi.any(),
-      operationId: joiRegex(opIdConvention),
-      tags: Joi.array().items(joiRegex(tagConvention)),
-      responses: Joi.object()
-        .pattern(statuses, Joi.object().keys({
+    paths: Joi.object().pattern(pathConvention,
+      Joi.object().pattern(verbs, Joi.object().keys({
+        summary: Joi.any(),
+        description: Joi.any(),
+        parameters: Joi.array()
+          .items(Joi.object().keys(parameterKeys)),
+        operationId: joiRegex(opIdConvention),
+        tags: Joi.array().items(joiRegex(tagConvention)),
+        responses: Joi.object().pattern(statuses, Joi.object().keys({
           description: Joi.any(),
           schema: Joi.any()
-        })
-      )
-    }))),
-    parameters: Joi.object().pattern(queryParamConvention, Joi.object().keys({
-      name: Joi.string(),
-      'in': Joi.string().insensitive().regex(/^query$/),
-      type: Joi.string(),
-      format: Joi.string(),
-      description: Joi.string()
-    })).pattern(pathParamConvention, Joi.object().keys({
-      name: Joi.string(),
-      'in': Joi.string().insensitive().regex(/^path/),
-      type: Joi.string(),
-      format: Joi.string(),
-      description: Joi.string()
-    })),
+        }))
+      }))
+    ),
+    parameters: Joi.object().pattern(/.*/, Joi.object().keys(parameterKeys)),
     definitions: Joi.any()
-
   });
 
   return schema;
@@ -142,17 +112,10 @@ function getSchema(checkStyle) {
 
 function validate(checkStyleFile, specFile) {
   Promise.join(getSpecPromise(specFile), getCheckStylePromise(checkStyleFile), 
-               function(spec, checkStyle) {
-    pathConvention = namingConventions[checkStyle.paths.namingConvention];
-    opIdConvention = namingConventions[checkStyle.paths.operationId.namingConvention];
-    errors = validateConventions(spec, pathConvention, opIdConvention);
-    console.log(errors)
-
-    return [spec,
-      getSchema(checkStyle),
-      {}];//{allowUnknown: true}];
-  }).spread(function(spec, schema, options) {
-    var result = Joi.validate(spec, schema, options);
+      function(spec, checkStyle) {
+    return [spec, getSchema(checkStyle)];
+  }).spread(function(spec, schema) {
+    var result = Joi.validate(spec, schema);
     if (result.error) {
       console.log(result.error) 
     }
